9ed739350de38fbb5fe9bea659e2257e7e3d3b17
Vaishnav17-S <vaishnav0417@gmail.com>
2026-05-25 18:14:35 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,8 @@
+INFO:     Will watch for changes in these directories: ['E:\\voice_analyzer\\backend']
+INFO:     Uvicorn running on http://0.0.0.0:8000 (Press CTRL+C to quit)
+INFO:     Started reloader process [16576] using WatchFiles
+INFO:     Started server process [20384]
+INFO:     Waiting for application startup.
+INFO:     Application startup complete.
+
 
